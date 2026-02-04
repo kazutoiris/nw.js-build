a8efb3df2f2c46fc57fed97a1e3d2cce37c6a635
Kazuto Iris <78157415+kazutoiris@users.noreply.github.com>
2026-02-04 10:35:12 +0000

    1

--- build.py
+++ build.py
@@ -29,10 +29,10 @@ def _run_build_process_timeout(cmd_input, timeout):
 def main():
     try:
         cmd_input = []
-        cmd_input.append("ninja -j8 -C out/nw nwjs")
-        cmd_input.append("ninja -j8 -C out/Release_x64 node")
-        cmd_input.append("ninja -j8 -C out/nw copy_node")
-        cmd_input.append("ninja -j8 -C out/nw dist")
+        cmd_input.append("ninja -C out/nw nwjs")
+        cmd_input.append("ninja -C out/Release_x64 node")
+        cmd_input.append("ninja -C out/nw copy_node")
+        cmd_input.append("ninja -C out/nw dist")
         _run_build_process_timeout(cmd_input, timeout=4*60*60)
         open(os.environ["GITHUB_OUTPUT"],"w").write("finish=true")
     except KeyboardInterrupt as e:
